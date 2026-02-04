59754f6b3c79fc525a56b85687d05ecf00284fa5
artasalija <salija.arta@gmail.com>
2026-02-05 00:25:29 +0100

    hamburger menu

--- wp-content/themes/fruskogorac/blocks/hero-section/hero-section.js
+++ wp-content/themes/fruskogorac/blocks/hero-section/hero-section.js
@@ -1,4 +1,10 @@
 // DOM content loaded event listener
 document.addEventListener('DOMContentLoaded', function() {
+    const hamburger= document.querySelector('.hamburger-menu');
+    const body = document.querySelector('body');
 
+hamburger.addEventListener('click', function(){
+    body.classList.toggle('menu-open');
+
+})
 });
