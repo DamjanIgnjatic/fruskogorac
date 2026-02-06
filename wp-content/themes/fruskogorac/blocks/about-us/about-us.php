8b51c3ab9ec0d38c059070110d2096fa98bd6f0c
DamjanIgnjatic <ignjatic.dev@gmail.com>
2026-02-06 22:19:55 +0100

    new about block

---
+++ wp-content/themes/fruskogorac/blocks/about-us/about-us.php
@@ -0,0 +1,29 @@
+<?php
+/* 
+* Block Name: Image
+* Post Type: page, post
+*/
+?>
+<?php if (isset($block['data']['previewImage'])): ?>
+    <?php do_action('block_image', __FILE__); ?>
+<?php else: ?>
+    <?php
+    // Include helpers
+    include get_template_directory() . '/template-parts/base/block-helper.php';
+
+    // Load values and assing defaults
+    $title = get_field('title') ?: false;
+    ?>
+    <section class="section about-us-section-wrapper block <?php echo $className; ?>"
+        <?php if ($bgUrl): ?>
+        style="background-image: url('<?php echo $bgUrl; ?>');"
+        data-desktop-image="<?php echo $bgUrl; ?>"
+        <?php endif; ?>
+        <?php if ($mobileBgUrl): ?>
+        data-mobile-image="<?php echo $mobileBgUrl; ?>"
+        <?php endif; ?>>>
+        <div class="container">
+            <!-- CONTENT -->
+        </div>
+    </section>
+<?php endif; ?>
